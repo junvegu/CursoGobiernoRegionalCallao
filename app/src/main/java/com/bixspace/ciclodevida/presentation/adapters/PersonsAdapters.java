3bc4bd63e8a3afa193dff650baf3c7850eb1bfe0
junvegu <juniorquegu@gmail.com>
2016-12-01 12:27:59 -0500

    Image

--- app/src/main/java/com/bixspace/ciclodevida/presentation/adapters/PersonsAdapters.java
+++ app/src/main/java/com/bixspace/ciclodevida/presentation/adapters/PersonsAdapters.java
@@ -1,8 +1,11 @@
 package com.bixspace.ciclodevida.presentation.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,7 +25,7 @@ public class PersonsAdapters extends RecyclerView.Adapter<PersonsAdapters.ViewHo
     private Context context;
 
     public PersonsAdapters(Context context, ArrayList<PersonaEntity> contactModelResponses
-                           ) {
+    ) {
         this.contactModelResponses = contactModelResponses;
         this.context = context;
     }
@@ -40,14 +43,25 @@ public class PersonsAdapters extends RecyclerView.Adapter<PersonsAdapters.ViewHo
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         final PersonaEntity model = contactModelResponses.get(position);
-        holder.tvName.setText(model.getNombres()+" "+model.getApellidos());
-        holder.tvAge.setText(model.getEdad()+"");
+        holder.tvName.setText(model.getNombres() + " " + model.getApellidos());
+        holder.tvAge.setText(model.getEdad() + "");
 
 
         /**
          * Aqui deberia existir algun algoritmo que permita transformar cadena a imagen
          */
-       // holder.imageView.setImageBitmap();
+
+        if (model.getFoto() != null) {
+            byte[] decodedString = Base64.decode(model.getFoto().getBytes(), Base64.DEFAULT);
+            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                holder.imageView.setImageBitmap(decodedByte);
+
+
+
+        }
+
+
+        // holder.imageView.setImageBitmap();
 
 
     }
@@ -56,7 +70,6 @@ public class PersonsAdapters extends RecyclerView.Adapter<PersonsAdapters.ViewHo
     public int getItemCount() {
         return contactModelResponses.size();
     }
-
 
 
     //El patron view holder permite inicializar los objetos traidos desde la vista del item (item_person)
@@ -69,7 +82,7 @@ public class PersonsAdapters extends RecyclerView.Adapter<PersonsAdapters.ViewHo
         public ViewHolder(View view) {
             super(view);
 
-            imageView = (ImageView) view.findViewById(R.id.iv_image);
+            imageView = (ImageView) view.findViewById(R.id.photo);
             tvAge = (TextView) view.findViewById(R.id.tv_age);
             tvName = (TextView) view.findViewById(R.id.tv_name);
         }
